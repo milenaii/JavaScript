2b46e8fa21a7f1816c782cd34bb1a8d5cadba57d
Milena Ivanova <milena_ii@abv.bg>
2017-12-08 13:22:39 +0200

    OddOrEven Task

---
+++ OddOrEven.js
@@ -0,0 +1,10 @@
+function oddOrEvenNumber (number){
+    // var number = numb[0];
+    if(number % 2 === 0){
+        console.log('even ' +  number)
+    }
+    else{
+        console.log("odd " + number)
+    }
+}
+// oddOrEvenNumber(['7']);
