4881fd10f8d565ecf3fdc6f7e24c21167eb316f4
Milena Ivanova <milena_ii@abv.bg>
2017-12-08 13:46:34 +0200

    divided by 5 and 7 task

---
+++ DivideBy7And5.js
@@ -0,0 +1,8 @@
+function numberDivededBy7And5(number){
+    if ((number%5 === 0 ) && (number %7 === 0)) {
+        console.log('true ' + number)
+    } else {
+        console.log('false ' + number)
+    }
+}
+// numberDivededBy7And5(['5'])
